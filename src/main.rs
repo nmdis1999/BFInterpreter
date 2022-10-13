796329778816e235d5b2d3f05037b34c0d1cfa07
Iti Shree <itishree1999@gmail.com>
2022-10-13 01:46:29 +0100

    some major updates

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::io;
 use std::io::Read;
-use std::process;
 
+static MAX_LEN: u32 = 40000;
+fn zeros(size: u32) -> Vec<u32> {
+    vec![0; size as usize]
+}
 fn main() {
     let file_path = env::args().nth(1).expect("Please pass *.bf input");
     println!("Reading file ...{}", file_path);
     //Read BF file to string prog
     let prog = fs::read_to_string(file_path).expect("Unable to read file");
     // without mut rust throw warning that vector is not mutable -> will read on it
-    let mut memory: Vec<u8> = vec![0];
+    let mut memory = zeros(MAX_LEN);
     let mut pc = 0;
     let mut dp = 0;
 
     let mut stack: Vec<usize> = Vec::new();
+
+    // One pass to find bracket pairs.
+    let brackets: HashMap<usize, usize> = {
+        let mut m = HashMap::new();
+        let mut scope_stack = Vec::new();
+        for (idx, ch) in prog.chars().enumerate() {
+            match ch {
+                '[' => {
+                    scope_stack.push(idx);
+                }
+                ']' => {
+                    m.insert(scope_stack.pop().unwrap(), idx);
+                }
+                _ => { /* ignore */ }
+            }
+        }
+
+        m
+    };
 
     loop {
         if pc >= prog.len() {
@@ -25,63 +48,51 @@ fn main() {
 
         match instrchar {
             '>' => {
-                dp += 1;
                 if dp >= memory.len() {
                     memory.push(0); //growing memory if needed
                 }
+                dp += 1;
             }
             '<' => {
-                if dp == 0 {
-                    eprintln!("pointer was at 0");
-                    process::exit(1);
+                if dp > 0 {
+                    dp -= 1;
                 }
-                dp -= 1;
             }
             '+' => {
-                memory[dp] += 1;
+                memory[dp] = memory[dp].wrapping_add(1);
+                if memory[dp] > 255 {
+                    memory[dp] = 0;
+                }
             }
             '-' => {
-                memory[dp] -= 1;
+                memory[dp] = memory[dp].wrapping_sub(1);
             }
             '[' => {
                 if memory[dp] == 0 {
-                    // find ']'
-                    while prog.as_bytes()[pc] as char != ']' {
-                        pc += 1;
-                        if pc >= prog.len() {
-                            eprintln!("no matching ']' for '['");
-                            process::exit(1);
-                        }
-                    }
+                    pc = brackets[&pc];
                 } else {
                     stack.push(pc);
                 }
             }
             ']' => {
-                // jump back to start of loop  if memory[dp] is not zero
+                let matching_bracket = stack.pop().unwrap();
                 if memory[dp] != 0 {
-                    pc = stack.pop().unwrap_or_else(|| {
-                        eprintln!("cannot find balancing opening bracket");
-                        process::exit(1);
-                    }) - 1; // sutract 1 so to re-process the '[' and add to stack
-                } else {
-                    // balanced so remove prev bracket
-                    stack.pop();
+                    pc = matching_bracket - 1;
                 }
             }
             '.' => {
-                print!("{}", memory[dp] as char);
+                let c = char::from_u32(memory[dp]).unwrap();
+                print!("{}", c);
             }
             ',' => {
                 let mut input = [0];
                 io::stdin()
                     .read_exact(&mut input)
                     .expect("error reading user input");
-                memory[dp] = input[0];
+                memory[dp] = input[0] as u32;
             }
             _ => {
-                eprintln!("unexpected error: character not supported");
-                process::exit(1);
+                //ignore
             }
         }
         pc += 1;
